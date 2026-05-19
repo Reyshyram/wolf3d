7d3ffd8329b6d09c44b1d744dbc0c15b894e26d1
Jérémy Delgrange <jeremy.delgrange@epitech.eu>
2026-05-20 00:02:29 +0200

    Feat: adding new sprites for the main menu buttons

--- include/menu.h
+++ include/menu.h
@@ -14,6 +14,8 @@
 typedef struct menu_data {
     sfSprite *bg;
     sfSprite *logo;
+    ui_button_t *play_btn;
+    ui_button_t *exit_btn;
 } menu_data_t;
 
 scene_t *menu_create(void);
@@ -23,5 +25,8 @@ void menu_draw(engine_t *engine);
 void handle_menu_events(engine_t *engine, sfEvent *event);
 void menu_on_exit(engine_t *engine);
 void init_sprites(engine_t *engine, menu_data_t *data);
+void init_buttons(engine_t *engine, menu_data_t *data);
+void on_play_click(void *data);
+void on_exit_click(void *data);
 
 #endif /* !MENU_H_ */
